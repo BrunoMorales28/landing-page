2758affe29e18190aa11b4975b609c25a5a34f61
Bruno Morales <bruno.morales@globallogic.com>
2023-12-12 16:52:14 -0300

    added public folder

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image"
 import Link from "next/link"
-import CONSTANTS from "@/constants"
+import {NAV_LINKS} from "@/constants"
+import Button from "../Button/Button"
 
 const NavBar = () => {
-    const home_navLink = CONSTANTS.NAV_LINKS[0]
-    const generalNavLinks = CONSTANTS.NAV_LINKS.slice(1)
+    const home_navLink = NAV_LINKS[0]
+    const generalNavLinks = NAV_LINKS.slice(1)
     return <nav className='border-2 flex-between max-container p-container relative z-30 py-5'>
+            <div className="lg:flexCenter hidden">
+                <Image
+                    src={NAV_LINKS[0].href}
+                    alt={NAV_LINKS[0].key}
+                />
+            </div>
             <ul className='hidden h-full gap-12 lg:flex '>
                     <Link href={home_navLink.href} className='regular-16 border-1  cursor-pointer pb-1.5 transition-all hover:font-bold'
                         >
@@ -24,6 +31,9 @@ const NavBar = () => {
                     )
                 }
             </ul>
+            <div className="lg:flexCenter hidden">
+                <Button onClick={()=>()=>{}} alt='' />
+            </div>
     </nav>
 }
 export default NavBar
